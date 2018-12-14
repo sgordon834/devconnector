cc16e4962e068eaaad1070bd6136afc155eaa183
sgordon834 <sgordon834@gmail.com>
2018-12-13 19:30:24 -0800

    testing registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -22,7 +22,7 @@ router.get('/test', (req, res) =>
   })
 );
 
-// @route GET api/users/register
+// @route POST api/users/register
 // @desc Register user
 // @access Public
 router.post('/register', (req, res) => {
